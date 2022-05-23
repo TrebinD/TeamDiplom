47eb93510590fe46e5a757601affa56dcb368d03
Dmitrii Trebin <d.trebin@rambler.ru>
2022-05-23 21:31:25 +0300

    Correct bags issue #8

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -13,18 +13,16 @@ public class PlayerTest {
     @Test
     public void shouldSumGenreIfOneGame() {
         GameStore store = new GameStore();
-        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game games = store.publishGame("Нетология Баттл Онлайн", "Аркады");
 
         Player player = new Player("Petya");
-        player.installGame(game);
-        player.play(game, 3);
+        player.installGame(games);
+        player.play(games, 3);
 
         int expected = 3;
-        int actual = player.sumGenre(game.getGenre());
+        int actual = player.sumGenre("Аркады");
         assertEquals(expected, actual);
     }
-
-    // другие ваши тесты
 
     @Test
     public void shouldSumGenreIfMoreThanOneGame() {
